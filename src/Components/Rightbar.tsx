9ffbe8cc7d8900fd03ee2ff357f0e9abd1650e92
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-10-13 17:40:22 +0530

    Rightbar Done

--- src/Components/Rightbar.tsx
+++ src/Components/Rightbar.tsx
@@ -1,10 +1,50 @@
-import { Box } from "@mui/material";
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from "@mui/material";
 import React from "react";
 
 const Rightbar = () => {
     return (
         <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}>
-            <Box position={"fixed"}>Rightbar</Box>
+            <Box position={"fixed"} width={350}>
+                <Typography variant="h6" paddingY={2} fontWeight={100}>
+                    Online Friends
+                </Typography>
+                <AvatarGroup max={7}>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                    <Avatar alt="someOne" src=""></Avatar>
+                </AvatarGroup>
+                <Typography variant="h6" paddingY={2} fontWeight={100}>
+                    Latest Photos
+                </Typography>
+                <ImageList cols={3} rowHeight={100} gap={5}>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                    <ImageListItem>
+                        <img src="https://www.bing.com/th?id=OHR.RioArazas_EN-IN1562638083_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&qlt=50" alt="someImage" />
+                    </ImageListItem>
+                </ImageList>
+            </Box>
         </Box>
     );
 };
