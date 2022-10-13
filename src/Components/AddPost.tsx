8bb04a58536fb91b7482f7a8e12ff93c09a726cf
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-10-13 20:01:11 +0530

    Added a form to add button

--- src/Components/AddPost.tsx
+++ src/Components/AddPost.tsx
@@ -1,15 +1,32 @@
 import { Add } from "@mui/icons-material";
-import { Tooltip, Fab } from "@mui/material";
+import { Tooltip, Fab, Modal, styled, Typography, TextField } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
+
+export const StyledModal = styled(Modal)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+});
 
 const AddPost = () => {
+    const [open, setOpen] = useState(false);
     return (
         <Box sx={{ display: { xs: "block", sm: "none" }, paddingLeft: "20px", left: { xs: "calc(50% - 45px)" } }} position="fixed" bottom={30}>
-            <Tooltip title="Add a Post">
+            <Tooltip title="Add a Post" onClick={() => setOpen(true)}>
                 <Fab color="primary" aria-label="add">
                     <Add />
                 </Fab>
             </Tooltip>
+
+            <StyledModal open={open} onClose={() => setOpen(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                <Box width={800} height={280} bgcolor={"white"} p={3} borderRadius={5} textAlign="center">
+                    <Typography variant="h6" color={"gray"}>
+                        Create Post
+                    </Typography>
+                    <TextField sx={{ width: "100%", color:"black" }} id="standard-multiline-static" label="What's on your Mind" multiline rows={4} variant="standard" />
+                </Box>
+            </StyledModal>
         </Box>
     );
 };
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,8 +1,11 @@
 import { Add, Home, Notes, People, Person, Storefront } from "@mui/icons-material";
-import { Box, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import React from "react";
+import { Box, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { StyledModal } from "./AddPost";
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <Box flex={1} sx={{ display: { xs: "none", sm: "block" }, paddingLeft: "20px" }}>
             <Box position={"fixed"}>
@@ -48,12 +51,20 @@ const Sidebar = () => {
                         </ListItemButton>
                     </ListItem>
                     <ListItem>
-                        <Button variant="contained">
+                        <Button onClick={() => setOpen(true)} variant="contained">
                             <ListItemIcon>
                                 <Add sx={{ color: "white" }} fontSize="large" />
                             </ListItemIcon>
                             <ListItemText primary="Add Post" />
                         </Button>
+                        <StyledModal open={open} onClose={() => setOpen(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                            <Box width={800} height={280} bgcolor={"white"} p={3} borderRadius={5} textAlign="center">
+                                <Typography variant="h6" color={"gray"}>
+                                    Create Post
+                                </Typography>
+                                <TextField sx={{ width: "100%", color: "black" }} id="standard-multiline-static" label="What's on your Mind" multiline rows={4} variant="standard" />
+                            </Box>
+                        </StyledModal>
                     </ListItem>
                 </List>
             </Box>
