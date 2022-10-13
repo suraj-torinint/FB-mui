b5650610a3a245ae370e0ec45b88cc8729fe36c3
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-10-13 13:24:44 +0530

    Sidebar done

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,10 +1,60 @@
-import { Box } from "@mui/material";
+import { Home, ModeNight, Notes, People, Person, Storefront } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
     return (
-        <Box flex={1} sx={{ display: { xs: "none", sm: "block" } }}>
-            Sidebar
+        <Box flex={1} sx={{ display: { xs: "none", sm: "block" }, paddingLeft: "20px" }}>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="/">
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Notes />
+                        </ListItemIcon>
+                        <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <People />
+                        </ListItemIcon>
+                        <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Storefront />
+                        </ListItemIcon>
+                        <ListItemText primary="Market Place" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Person />
+                        </ListItemIcon>
+                        <ListItemText primary="Friends" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ModeNight />
+                        </ListItemIcon>
+                        <Switch />
+                    </ListItemButton>
+                </ListItem>
+            </List>
         </Box>
     );
 };
